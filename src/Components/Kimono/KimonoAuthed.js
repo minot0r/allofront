916f6962a1141c596ce8bda4fd7e24dd0b539a8b
Valentin Giorgetti <giorgettivalentin@gmail.fr>
2022-02-15 00:12:03 +0100

    fixed authed comp

--- src/Components/Kimono/KimonoAuthed.js
+++ src/Components/Kimono/KimonoAuthed.js
@@ -3,6 +3,6 @@ import Login from "../Login/Login";
 
 export default function KimonoAuthed({ children, reverse }) {
   let loggedIn = useSelector((state) => state.auth.loggedIn);
-  if (reverse && loggedIn) return <>{children}</>;
+  if (reverse && !loggedIn) return <>{children}</>;
   return loggedIn ? <>{children}</> : <Login />;
 }
